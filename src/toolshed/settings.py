28178b0aa86233637dd5e6182138ca7fe31427cf
Joshua Kropf <josh.kropf23@aggiemail.usu.edu>
2020-04-05 22:26:10 -0600

    Modified the Admin view. Added a link to the admin view if the user is authenticated as a superuser on the navbar. Need to fix a few style things like bullet points in the nav bar.

--- src/toolshed/settings.py
+++ src/toolshed/settings.py
@@ -31,13 +31,13 @@ INSTALLED_APPS = [
     'backend.apps.BackendConfig',
     'crispy_forms',
     'phonenumber_field',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'localflavor',
+	'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
